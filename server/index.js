995dac4a554a1bc8b09e7a5bcfb9115ac1f0a175
Dileep Thomas <dileep.thomas@tesco.com>
2019-02-28 18:26:57 +0530

    Dileep Thomas || serving code moved to last

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.get('/api/data', (req, res) => {
+    res.send({data: "Success"})
+})
+
 app.use(express.static(path.join(__dirname,  'build')));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '..', 'app', 'build', 'index.html'));
 });
-
-app.get('/api/data', (req, res) => {
-    res.send({data: "Success"})
-})
 
 app.listen(4000);
